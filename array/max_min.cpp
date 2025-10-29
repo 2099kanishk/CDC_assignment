6de59ca9bf6eb5833981e7c479fe27a722868ef1
2099kanishk <kanishkbarnala9911@gmail.com>
2025-10-29 23:37:59 +0530

    update

--- array/max_min.cpp
+++ array/max_min.cpp
@@ -1,5 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main{
-    
+int main() {
+    cout << "enter the size of the array"  << endl ;
+    int n;
+    cin >> n ; 
+    vector<int> nums(n) ;
+    cout <<"enter the elements of the array" << endl ;
+    for(int i = 0 ; i< n  ; i++){
+        cin >> nums[i] ;
+    }
+    int maxnum = nums[0] ;
+    int minnum = nums[0] ;
+    for(int i = 0 ; i < n ; i++){
+      if(nums[i] > maxnum){
+        maxnum = nums[i];
+      }
+      if(nums[i] < minnum) {
+        minnum = nums[i] ;
+      }
+    }
+    cout <<"maxnum is " << maxnum << endl ;
+    cout << "minnum is " << minnum << endl ;
+
+
+    return 0 ;
+
+
+
 }
