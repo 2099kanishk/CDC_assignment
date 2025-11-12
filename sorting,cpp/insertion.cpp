032fee0f078aef946a29351de256b49dd117befa
2099kanishk <kanishkbarnala9911@gmail.com>
2025-11-12 22:00:43 +0530

    update

---
+++ sorting,cpp/insertion.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> insertion_sort(vector<int> &nums , int n){
+    for(int i = 0 ; i < n-1 ; i++){
+        int j = i ;
+        while ( j > 0 ; arr[j-1] >arr[j]){
+            swap (arr[j-1] , arr[j]) ;
+            j-- ;
+        }
+}
+return nums ;
+}
