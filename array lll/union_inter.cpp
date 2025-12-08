64f7022ce6094b7c754dd5bd590a6680dd264c91
2099kanishk <kanishkbarnala9911@gmail.com>
2025-12-08 20:41:34 +0530

    update

---
+++ array lll/union_inter.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int union_inter(vector<int> &arr1 , vector<int> &arr2)
+{
+    sort(arr1.begin(), arr1.end());
+    sort(arr2.begin(), arr2.end());
+    int i = 0, j = 0;
+    vector<int> ans ;
+    while ( i < arr1.size() && j < arr2.size()){
+        if(arr[i] < arr[j]){
+            i++ ;
+        }
+        else if(arr[j] < arr[i]){
+            j++ ;
+        else{
+            if(ans.empty )
+        }        }
+    }
+} 
